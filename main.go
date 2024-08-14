cb56ffa58ef75831c7a31ebeedff4fcc9f8523d1
Khushi Sinha <khushisinha4786@gmail.com>
2024-08-14 08:41:31 +0530

    Added main.go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlers.IndexHandler)
+	http.HandleFunc("/upload", handlers.UploadHandler)
+	http.HandleFunc("/files/", handlers.FileHandler)
+	http.HandleFunc("/download", handlers.DownloadHandler)
+
+	fmt.Println("Serving at port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
